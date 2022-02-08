a2ffdd4cbb97d51855ca5cd8e4be2e8c9a3ada04
Pasang Nuru Sherpa <79486506+solu22@users.noreply.github.com>
2022-02-08 08:31:06 +0200

    exe 9.1

---
+++ bmiCalculator.ts
@@ -0,0 +1,19 @@
+type bmiMsg = string;
+let msg = "";
+
+const calculateBmi = (height: number, weight: number): bmiMsg => {
+  const bmiResult = weight / ((height * 0.01) ^ 2);
+
+  if (bmiResult < 18.5) return (msg += "Underweight");
+  if (bmiResult >= 18.5 && bmiResult <= 24.9)
+    return (msg += "Normal(healthy weight)");
+  if (bmiResult >= 25 && bmiResult <= 29.9) return (msg += "Overweight");
+  else {
+    return (msg += "More Overweight");
+  }
+};
+
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
+
+console.log(calculateBmi(height, weight));
